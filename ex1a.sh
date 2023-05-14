0ae845afcf8edac5ee990ccc084837c3e57ab48e
MosheNamdar <110346394+MosheNamdar@users.noreply.github.com>
2023-05-15 00:14:19 +0300

    first ex

---
+++ ex1a.sh
@@ -0,0 +1,14 @@
+echo "Moshe Namdar" > my_name.txt
+mkdir ~/new
+cp name.txt ~/new
+mv ~/new/name.txt ~/new/MosheNamdar.txt
+gedit ~/new/MosheNamdar.txt
+#MosheNamdar
+#Animal: giraffe
+cp ~/new/MosheNamdar.txt ~/new/newFile.txt
+cd ~/new
+ls
+cd
+#another option:
+cd ~
+rmdir ~/new
