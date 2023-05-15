1dfdb7760c54031b25c6e032fc4c6cb43169307d
MosheNamdar <110346394+MosheNamdar@users.noreply.github.com>
2023-05-15 14:08:44 +0300

    more exs

---
+++ ex1b.sh
@@ -0,0 +1,35 @@
+#1
+ls - S
+
+#1a
+ls -S /etc/ | head -1 
+#Then I found the biggest file that is "services"
+
+#1b
+cat /etc/services
+
+#2
+ls -S -r
+
+#2a
+ls -S /etc/ | tail -1
+#Then I found the smallest file that is "motd"
+cat /etc/motd
+
+#3
+mkdir -p grandpa/father/son
+
+#4
+bc
+
+#5
+bc -q
+
+#6
+cal 5 2023
+ 
+#6a
+cal 2023
+
+
+
